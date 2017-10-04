14338450ac19d0e4f2d441cf1a4de633dd4922eb
TresLos <asistare7@gmail.com>
2017-10-04 16:42:08 +0200

    Feta la llibreria acabada de vectors

--- Util/vector.c
+++ Util/vector.c
@@ -22,12 +22,9 @@
  * 3.1		-indexat		Recuperem l'element indicat
  * 3.2		-indexat invers		Recuperem l'element indicat
  * 3.3		-modifica		Edita l'element indicat
- * 3.4		-modifica tamany	Canvia el us sense afegir dades
+ * 3.4		-modifica dimensió	Canvia el us sense afegir dades
  *
  **/
-void* vector_retorn_element (struct vector *v, size_t i);
-void* vector_retorn_element_reves (struct vector *v, size_t i);
-void vector_modifica_element (struct vector *v, size_t i, void *dada);
 
 	/*1*	Poder Inicialitzar i finalitzar l'estructura	**/
 /* Totes les dades necessàries per a poder generar l'estructura */
@@ -85,18 +82,26 @@ void vector_alliberar (struct vector *v)
 // En cas de no poder-ho fer, destrozarà directament el programa "exit".
 // Testeixat, 0→1, 1→2 i sempre incrementa o iguala la diferència amb
 // l'anterior, cosa que permet assegurar que realment incrementa.
-void incrementar_capacitat_del_vector (struct vector *v)
+void incrementar_capacitat_del_vector_en_necessitat (struct vector *v)
 {
+	// No cal fer res.
+	if (v->us < v->capacitat) return;
+
 	// Valor extret de open source de ArrayList de Java.
-	v->capacitat = (v->us * 3)/2 +1;
+	v->capacitat = (v->capacitat * 3)/2 +1;
+
+	// Assegura tamany mínim.
+	if (v->capacitat <= v->us)
+		v->capacitat = v->us +1;
+
 	v->dada = realloc (v->dada, v->capacitat * v->byts);
 
 	// Parem en cas d'error.
 	if (!v->dada)
 	{
 		printf ("%s\nCapacitat: %zu, Nombre de byts: %zu\n%s\n",
-		"Error al redefinir la capacitat del vector.", v->capacitat, v->byts,
-		"Error generat pel realloc!");
+		"Error al redefinir la capacitat del vector.", v->capacitat,
+		v->byts, "Error generat pel realloc!");
 		exit (EXIT_FAILURE);
 	}
 }
@@ -125,8 +130,7 @@ void assegurar_memoria_minima (struct vector *v)
 void vector_afegir (struct vector *v, void* dada)
 {
 	// Cas en que no hi hagi més capacitat.
-	if (v->us == v->capacitat)
-		incrementar_capacitat_del_vector (v);
+	incrementar_capacitat_del_vector_en_necessitat (v);
 
 	// Emmagatzemem la informació.
 	memcpy (v->dada + v->us++*v->byts, dada, v->byts);
@@ -147,7 +151,7 @@ void* vector_treure (struct vector *v)
 /*2.5*		-mostra			Mostra l'últim element		**/
 /* Mostra l'últim element afegit */
 // En cas de no haver cap element, aturarà el programa.
-void* vector_treure (struct vector *v)
+void* vector_mostra (struct vector *v)
 {
 	// Comprovem que tingui mínim un element.
 	assegurar_memoria_minima (v);
@@ -159,14 +163,60 @@ void* vector_treure (struct vector *v)
 	/*3*	Treballar com si fos una llista				**/
 /* Totes les operacions que podriem demanar a una pila */
 // Afegir element, mirar o treure un element.
-/*
- * 3.0		>cercar			Assegura que sigui vàlid
- * 3.1		-indexat		Recuperem l'element indicat
- * 3.2		-indexat invers		Recuperem l'element indicat
- * 3.3		-modifica		Edita l'element indicat
- * 3.4		-modifica tamany	Canvia el us sense afegir dades
- *
-void* vector_retorn_element (struct vector *v, size_t i);
-void* vector_retorn_element_reves (struct vector *v, size_t i);
-void vector_modifica_element (struct vector *v, size_t i, void *dada);
- **/
+
+/*3.0*		>cercar			Assegura que sigui vàlid	**/
+/* Comprova que l'índex donat no està fora dels elements usats */
+// En cas de no fer un bon ús, pararà el programa.
+void error_cercar_un_element (struct vector *v, size_t i)
+{
+	if ( i >= v->us )
+	{
+		printf ("%s\n%s%zu, %s%zu\n%s\n",
+		"Error al retornar un element.",
+		"Elements usats: ", v->us,
+		"Element demanat ", i,
+		"Error generat al demanar un element fora de lloc"
+		);
+
+		exit (EXIT_FAILURE);
+	}
+}
+
+/*3.1*		-indexat		Recuperem l'element indicat	**/
+/* Donat un índex, retornarà l'element dins la llista */
+// Capacitat d'aturar el programa.
+void* vector_retorn_element (struct vector *v, size_t i)
+{
+	// Comprova el cas de que no existeixi l'element.
+	error_cercar_un_element (v, i);
+
+	// On està emmagatzemat.
+	return v->dada + i*v->byts;
+}
+
+/*3.2*		-indexat invers		Recuperem l'element indicat	**/
+/* Donat un índex, retornarà l'element dins la llista comptant del reves */
+// Capacitat d'aturar el programa.
+void* vector_retorn_element_reves (struct vector *v, size_t i)
+{
+	// Comprova el cas de que no existeixi l'element.
+	error_cercar_un_element (v, i);
+
+	// On està emmagatzemat.
+	return v->dada + (v->us - i -1)*v->byts;
+}
+
+/*3.3*		-modifica		Edita l'element indicat		**/
+/* Permet modificar un element amb la informació de dada */
+// Capacitat d'aturar el programa.
+void vector_modifica_element (struct vector *v, size_t i, void *dada)
+{ memcpy (vector_retorn_element(v,i), dada, v->byts); }
+
+/*3.4*		-modifica dimensió	Canvia el us sense afegir dades	**/
+/* Permet modificar rapidament el tamany de la llista */
+// Capacitat d'aturar el programa.
+void vector_modifica_us (struct vector *v, size_t us)
+{
+	v->us = us;
+	incrementar_capacitat_del_vector_en_necessitat (v);
+}
---
+++ Util/vector.h
@@ -0,0 +1,34 @@
+#ifndef DEFINICIO_PER_EXECUCIO_H_
+#define DEFINICIO_PER_EXECUCIO_H_
+
+#include <stdlib.h>
+
+// estructura per tenir la llista.
+struct
+vector
+{
+	size_t us;
+	size_t capacitat;
+	size_t byts;
+	void * dada;
+};
+
+/* Inicialitzar i finalitzar la llista */
+struct
+vector vector_inicialitzar (size_t capacitat_inicial, size_t byts_per_element);
+void vector_alliberar (struct vector *v);
+
+/* Instruccions de pila */
+void  vector_afegir (struct vector *v, void* dada);
+void* vector_treure (struct vector *v);
+void* vector_mostra (struct vector *v);
+
+/* Instruccions de llista */
+void* vector_retorn_element (struct vector *v, size_t i);
+void* vector_retorn_element_reves (struct vector *v, size_t i);
+void vector_modifica_element (struct vector *v, size_t i, void *dada);
+
+/* Instrucció per modificar elements usats */
+void vector_modifica_us (struct vector *v, size_t nou_us);
+
+#endif // DEFINICIO_PER_EXECUCIO_H_
